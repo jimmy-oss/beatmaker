6f5ee742eb13e45c4f07945421815cd7467084f5
jimmy-oss <jimmynjonge51@gmail.com>
2021-11-23 01:22:28 +0300

    looping the pads play and stop

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ class Drumkit{
              this.hihatAudio = document.querySelector('.hihat-sound');
              this.index = 0; // This is gonna track our audio
              this.bpm = 150;
+             this.isPlaying = null;
        }
        activePad(){
           this.classList.toggle("active");
@@ -17,17 +18,46 @@ class Drumkit{
            // Loop over the pads
            activeBars.forEach(bar => {
                 bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+                // check if the pads are active 
                 if(bar.classList.contains('active')){
-                  
+                 // check each sound
+                 if(bar.classList.contains('kick-pad')){
+                  this.kickAudio.currentTime = 0;
+                    this.kickAudio.play();
+                 }
+                 if(bar.classList.contains('snare-pad')){
+                   this.snareAudio.currentTime = 0;
+                  this.snareAudio.play();
+               }
+               if(bar.classList.contains('hihat-pad')){
+                this.hihatAudio.currentTime = 0;
+                this.hihatAudio.play();
+             }
                 }
            });
           this.index++;
     }
     start(){
       const interval = (60/this.bpm) * 1000;
-        setInterval(() =>{
-           this.repeat();
+      // check if it's playing
+      if(!this.isPlaying){
+      this.isPlaying = setInterval(() =>{
+               this.repeat();
         }, interval)
+      }else{
+        // clear the interval
+          clearInterval(this.isPlaying);
+          this.isPlaying = null;
+      }
+    }
+    updateBtn(){
+      if(!this.isPlaying){
+        this.playBtn.innerText = 'stop';
+        this.playBtn.classList.add = ('active');
+      }else{
+        this.playBtn.innerText = 'play';
+        this.playBtn.classList.add = ('active');
+      }
     }
 }
   const drumkit = new Drumkit ();
